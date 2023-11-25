d56f6c9096d573526f5db521089f0ece780dd8b0
Todd <todd.fountain39@gmail.com>
2023-11-25 16:51:59 -0600

    transaction tracking and withdrawl validation added with new drivers

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ class Account {
 
   constructor(username) {
     this.username = username;
-    this.balance = 50;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
   }
 
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
+
 class Transaction {
 
   constructor(amount, account) {
@@ -13,7 +24,10 @@ class Transaction {
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 }
 
@@ -21,7 +35,9 @@ class Deposit extends Transaction {
   get value() {  
     return this.amount
   }
-
+  isAllowed() {
+    return true;
+  }
 }
 
 class Withdrawal extends Transaction {
@@ -29,26 +45,34 @@ class Withdrawal extends Transaction {
   get value() {  
     return -this.amount
   }
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account("snow-patrol");
+const myAccount = new Account();
 
-console.log('Starting Balance:', myAccount.balance);
+console.log('Starting Account Balance: ', myAccount.balance);
 
-t1 = new Withdrawal(50.25, myAccount);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
 
-console.log('Balance:', myAccount.balance);
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
