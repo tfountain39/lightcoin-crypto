fac931c3353ad02732dbcc02bc3a43742043d60a
Todd <todd.fountain39@gmail.com>
2023-11-25 16:21:33 -0600

    Creation and implementation of account class

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-let balance = 500.00;
-
 class Withdrawal {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
     this.account = account;
   }
 
   commit() {
-    balance += this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
 
 class Deposit {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
     this.account = account;
   }
 
   commit() {
-    balance += this.amount;
     this.account.balance += this.amount;
   }
 
@@ -31,8 +28,7 @@ class Account {
 
   constructor(username) {
     this.username = username;
-    // Have the account balance start at $0 since that makes more sense.
-    this.balance = 0;
+    this.balance = 50;
   }
 
 }
@@ -42,16 +38,16 @@ class Account {
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
